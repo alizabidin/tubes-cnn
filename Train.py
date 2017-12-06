3de682bfaf970f3e2bb7f56ce8d9256b76bd0c2c
alizabidin <34295323+alizabidin@users.noreply.github.com>
2017-12-06 17:02:37 +0700

    Update Train.py

--- Train.py
+++ Train.py
@@ -4,5 +4,5 @@ x, y = load_dataset('train')
 x_val, y_val = load_dataset('validation')
 
 # proses training, parameter, data augmentasi dan penyimpanan model ada di dalam fungsi train
-# default lr = 1e-3, epoch = 10, data augmentasi horizontal flip, save model = False
+# default lr = 1e-3, epoch = 50, data augmentasi horizontal flip, save model = False
 train(x, y, x_val=x_val, y_val=y_val, save_model=True)
